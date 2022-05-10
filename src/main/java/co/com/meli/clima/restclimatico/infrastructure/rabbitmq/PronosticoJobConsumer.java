70b49e223a0a48f2dae052a357ffba98229c1324
William Sanabria <williamsanabria@72B-TI-MAC-02.local>
2022-05-10 11:21:44 -0500

    init

--- src/main/java/co/com/meli/clima/restclimatico/infrastructure/rabbitmq/PronosticoJobConsumer.java
+++ src/main/java/co/com/meli/clima/restclimatico/infrastructure/rabbitmq/PronosticoJobConsumer.java
@@ -3,6 +3,7 @@ package co.com.meli.clima.restclimatico.infrastructure.rabbitmq;
 import co.com.meli.clima.restclimatico.application.services.CalcularPronostico;
 import co.com.meli.clima.restclimatico.domain.entity.Pronostico;
 import co.com.meli.clima.restclimatico.infrastructure.repository.PronosticoRepository;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -15,18 +16,19 @@ public class PronosticoJobConsumer {
 
     private CalcularPronostico calcularPronostico;
 
-    public PronosticoJobConsumer(PronosticoRepository repository){
+    public PronosticoJobConsumer(PronosticoRepository repository, CalcularPronostico calcularPronostico){
         this.repository = repository;
+        this.calcularPronostico = calcularPronostico;
     }
 
     @RabbitListener(queues = "${pronostico.amqp.queue}")
-    public void procesarPronostico(String message){
+    public void procesarPronostico(@NotNull String message){
         log.info("Consumer> " + message);
         if(message.contains(":")) {
             String[] array = message.split(":");
             log.info(" [x] Received '" + message + "'");
             try {
-                calcularPronostico.realizarPronostico(Integer.getInteger(array[0]), Integer.getInteger(array[1]));
+                this.calcularPronostico.realizarPronostico(Integer.getInteger(array[0]), Integer.getInteger(array[1]));
             }catch (Exception e){
                 log.warn("No se puede ejecutar el pronostico");
             }
