6ca5f2951e2879f06e4cfc9681c84224391eb73f
William Sanabria <williamsanabria@72B-TI-MAC-02.local>
2022-05-10 10:38:04 -0500

    init

--- src/main/java/co/com/meli/clima/restclimatico/infrastructure/rabbitmq/PronosticoJobConsumer.java
+++ src/main/java/co/com/meli/clima/restclimatico/infrastructure/rabbitmq/PronosticoJobConsumer.java
@@ -1,5 +1,6 @@
 package co.com.meli.clima.restclimatico.infrastructure.rabbitmq;
 
+import co.com.meli.clima.restclimatico.application.services.CalcularPronostico;
 import co.com.meli.clima.restclimatico.domain.entity.Pronostico;
 import co.com.meli.clima.restclimatico.infrastructure.repository.PronosticoRepository;
 import org.slf4j.Logger;
@@ -12,13 +13,24 @@ public class PronosticoJobConsumer {
     private Logger log = LoggerFactory.getLogger(PronosticoJobConsumer.class);
     private PronosticoRepository repository;
 
+    private CalcularPronostico calcularPronostico;
+
     public PronosticoJobConsumer(PronosticoRepository repository){
         this.repository = repository;
     }
 
     @RabbitListener(queues = "${pronostico.amqp.queue}")
-    public void procesarPronostico(String pronostico){
-        log.info("Consumer> " + pronostico);
-        log.info("ToDo created> " + this.repository.findById(1));
+    public void procesarPronostico(String message){
+        log.info("Consumer> " + message);
+        if(message.contains(":")) {
+            String[] array = message.split(":");
+            log.info(" [x] Received '" + message + "'");
+            try {
+                calcularPronostico.realizarPronostico(Integer.getInteger(array[0]), Integer.getInteger(array[1]));
+            }catch (Exception e){
+                log.warn("No se puede ejecutar el pronostico");
+            }
+        }
+        
     }
 }
